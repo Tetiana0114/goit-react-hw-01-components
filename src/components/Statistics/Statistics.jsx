2ed324a6b097e3637c14bee0d2500e935660f635
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-10-09 23:45:47 +0300

    Add styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css'
 
 export const Statistics = ({ title, stats }) => {
-    return <section className="statistics">
-  <h2 className="title">{title}</h2>
+    return <section className={css.statistics}>
+  <h2 className={css.title}>{title}</h2>
 
-  <ul className="stat-list">
+  <ul className={css.stat_list}>
     {stats.map(stat => (
-      <li key={stat.id} className="item">
-      <span className="label">{stat.label}: </span>
-      <span className="percentage">{stat.percentage}%</span>
+      <li key={stat.id} className={css.item}>
+      <span className={css.label}>{stat.label} </span>
+      <span className={css.percentage}>{stat.percentage}%</span>
     </li>))}
   </ul>
   </section>
