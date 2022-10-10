ceb5790dad87d70718d09cf703009d7d06207986
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-10-10 23:47:22 +0300

    Add new styles

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import css from './FriendList.module.css'
 
 import {FriendListItem} from 'components/Friends/FriendListItem';
 
 export const FriendList = ({ friends }) => {
-    return <ul className="friend-list" >
+    return <ul className={css.friend_list}>
          {friends.map(friend => (
          <FriendListItem key={friend.id} friends={friend}/>
          ))}
--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -1,10 +1,11 @@
 // import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css'
 
 export const FriendListItem = ({ friends: { avatar, name, isOnline } }) => {
-    return <li className="item">
-    <span className="status">{isOnline}</span>
-    <img className="avatar" src={avatar} alt="User avatar" width="48" />
-    <p className="name">{name}</p>
+    return <li className={css.item}>
+    <span className={css.status}>{isOnline}</span>
+    <img className={css.avatar} src={avatar} alt="User avatar" width="60" />
+    <p className={css.name}>{name}</p>
   </li>
   };
     
